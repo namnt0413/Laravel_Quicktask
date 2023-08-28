62ee4c8202a8fe2baa33ebcf8dd128dc5ce6f19e
namnt0413 <namnt0413@gmail.com>
2023-08-28 10:24:15 +0700

    create relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
@@ -16,4 +17,9 @@ class Task extends Model
         'user_id',
         'deadline',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
